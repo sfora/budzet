5678b57fb9d833cffb621c17e92349fcca402e22
sfora <sfora14@gmail.com>
2014-11-01 21:29:42 +0100

    powoli

---
+++ logika.js
@@ -0,0 +1,42 @@
+Zakup.prototype.constructor = Zakup;
+Budzet.prototype.constructor = Budzet;
+
+function Zakup(co,poile){
+    var datan = new Date()
+    var dzien = datan.getDate();
+    var dzient = datan.getDay();
+    var godzina = datan.getHours();
+    var minuta = datan.getMinutes();
+    var sekunda = datan.getSeconds();
+    var miesiac = datan.getMonth();
+    var rok = datan.getFullYear();
+    var co = co;
+    var poile = poile;
+    var id = datan.getTime();
+    
+}
+
+function Budzet(){
+var budzetdoc;
+    var budzetdzis;
+    var budzetweek;
+    var budzetmonth;
+    var wyswietlane = [];
+    var sortyjakies = [];
+
+
+
+
+
+
+}
+
+    
+    
+Budzet.prototype.dodajzakup = function (){
+
+
+
+
+
+};
