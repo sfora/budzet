a477f9d908b6043540a732fb61099755758d845f
sfora <sfora14@gmail.com>
2014-11-05 23:14:28 +0100

    logika

--- logika.js
+++ logika.js
@@ -21,7 +21,7 @@ var budzetdoc;
     var budzetdzis;
     var budzetweek;
     var budzetmonth;
-    this.wyswietlane = [];
+    this.wyswietlane = new Array();
     this.sortyjakies = [];
 }
 
@@ -35,13 +35,60 @@ var elempoile = document.getElementById("poile").value;
   eleme.style.zIndex = "-3";
   eleme.style.top = "-1200px";
     var zakupmaly = new Zakup(elemco,elempoile);
-    console.log(zakupmaly.dzien + " " + zakupmaly.co + " " + zakupmaly.poile);
+    budzet.wyswietlane.push(zakupmaly);
+    var elemdodawany = document.createElement('div');
+    elemdodawany.classList.add('dupsko');
+    elemdodawany.setAttribute('id',zakupmaly.id);
+    var parelem = document.getElementById('tenglowny');
+    var paradodawany = document.createElement('p');
+    paradodawany.classList.add('pklas');
+    paradodawany.innerHTML = elemco + " " + elempoile;
+    var kolkodod = document.createElement('div');
+    kolkodod.classList.add('plusnew');
+    var pluspoz = document.createElement('div');
+    pluspoz.classList.add('poziom');
+    var pluspion = document.createElement('div');
+    pluspion.classList.add('pion');
+    kolkodod.appendChild(pluspion);
+    kolkodod.appendChild(pluspoz);
+    elemdodawany.appendChild(paradodawany);
+    elemdodawany.appendChild(kolkodod);
+    parelem.appendChild(elemdodawany);
+    
+    
+    
     
 
 
 
 
 };
+
+//Przypominacz.prototype.wyswietlMemo = function (notka) {
+//    var elem = document.createElement("div");
+//    elem.setAttribute("class", "dupaclass");
+//    elem.setAttribute("id", notka.idnad);
+//    var xpose = notka.posx + "px";
+//    var ypose = notka.posy + "px";
+//    elem.style.left = xpose;
+//    elem.style.top = ypose;
+//    elem.style.backgroundColor = 'rgba(' + notka.kolor.r + ',' + notka.kolor.g + ',' + notka.kolor.b + ',' + notka.kolor.a + ')';
+//    var parelem = document.getElementById('glowny');
+//    parelem.appendChild(elem);
+//    var parag = document.createElement("p");
+//    parag.setAttribute("class", "parag");
+//    parag.setAttribute("id", elem.id + "c");
+//    parag.innerHTML = notka.tytul;
+//    parag.addEventListener('click', memor.killmemo, false);
+//    elem.appendChild(parag);
+//    var paragtim = document.createElement("p");
+//    paragtim.setAttribute("class", "paragtime");
+//    paragtim.setAttribute("id", elem.id + "c");
+//    paragtim.innerHTML = notka.datalancuch;
+//    paragtim.addEventListener('click', memor.killmemo, false);
+//    elem.appendChild(paragtim);
+//};
+
 
 function dupa(nowy){
   var elem=document.getElementById('kolko1');
